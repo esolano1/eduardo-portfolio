1e7895d3c73726b13393b5b6f47cad8b6452796a
esolano1 <joeybello23@gmail.com>
2026-04-28 22:29:37 -0600

    feat: add one more link of a page I created

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ export default function ProjectsPage() {
   return (
     <main className="p-6">
       <h1 className="text-2xl font-semibold">All my projects</h1>
-      <p>Page in construction</p>
+      <p>Project for an automotive service place: <a href="https://esolano1.github.io/AL-Detallado-Automotriz/">A-L Detallado Automotriz</a></p>
       <p>Project for a snacks place: <a href="https://esolano1.github.io/EduardoStore/">Burger House</a></p>
       <p>Project for coffee shop: <a href="https://esolano1.github.io/CafeRivera/">Café Rivera</a></p>
       <p>Project in DEV.F: <a href="https://eduardo-proyecto-final-dev-f-8rde.vercel.app/">Page for products using an API</a></p>
